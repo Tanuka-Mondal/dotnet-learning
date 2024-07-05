4117fef7b77007f46f4b6544b16506752def36ec
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2024-07-05 19:25:52 +0530

    Add files via upload

---
+++ Bicycle-Web_Tanuka/Context/BicycleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bicycle_Web_Tanuka.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bicycle_Web_Tanuka.Context
+{
+    public class BicycleDbContext : DbContext
+    {
+        public BicycleDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Bicycle> Bicycles { get; set;}
+    }
+}
